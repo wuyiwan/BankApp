ddc77144a7ee5cfb7b9b7e1a84491ad84fc5d76d
newwan <9678702+newwan@user.noreply.gitee.com>
2026-04-21 21:50:46 +0800

    feat(配置): 添加交易配置类支持SQLite数据库事务管理
    添加TransactionConfig类用于配置SQLite数据库连接和事务管理

---
+++ src/main/java/com/Scoders/BankingApp/config/TransactionConfig.java
@@ -0,0 +1,30 @@
+package com.Scoders.BankingApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+
+    private static final String DATABASE_URL = "jdbc:sqlite:bank.db";
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl(DATABASE_URL);
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
